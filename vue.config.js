51f88943e682c77336eeafc4d2ec1e48184679de
Hank Shen <swhbox@foxmail.com>
2021-03-08 14:47:00 +0800

    开启gzip压缩

--- vue.config.js
+++ vue.config.js
@@ -6,7 +6,7 @@ if (process.env.NODE_ENV === 'production') {
   const CompressionPlugin = require('compression-webpack-plugin')
   webpackPlugins.push(new CompressionPlugin({
     algorithm: 'gzip',
-    test: /\.js$|\.html$|\.css$|\.jpg$|\.jpeg$|\.png/, // 需要压缩的文件类型
+    test: /\.js$|\.html$|\.css$/, // 需要压缩的文件类型
     threshold: 10240, // 归档需要进行压缩的文件大小最小值，我这个是10K以上的进行压缩
     deleteOriginalAssets: false, // 是否删除原文件
     minRatio: 0.5
